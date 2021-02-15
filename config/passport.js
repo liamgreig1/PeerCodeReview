c529ca6dad2e7df277a91ac49868f58808b7e942
liamgreig1 <liam.greig1998@googlemail.com>
2021-02-15 12:04:16 +0000

    PassportConfig Creation

--- config/passport.js
+++ config/passport.js
@@ -1,18 +1,46 @@
-// const JwtStrategy = require('passport-jwt').Strategy
-// const ExtractJwt = require('passport-jwt').ExtractJwt;
+const JwtStrategy = require('passport-jwt').Strategy
+const ExtractJwt = require('passport-jwt').ExtractJwt;
 const fs = require('fs');
 const path = require('path');
 const User = require('mongoose').model('user');
 
+// Go up one directory, then look for file name
 const pathToKey = path.join(__dirname, '..', 'id_rsa_pub.pem');
+
+// The verifying public key
 const PUB_KEY = fs.readFileSync(pathToKey, 'utf8');
 
-// TODO
+// At a minimum, you must pass the `jwtFromRequest` and `secretOrKey` properties
 const options = {
-    // jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-    // secretOrKey: PUB_KEY,
-    // algorithms: ['RS256']
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: PUB_KEY,
+  algorithms: ['RS256']
 };
 
-// TODO
-module.exports = (passport) => {}
+// app.js will pass the global passport object here, and this function will configure it
+module.exports = (passport) => {
+    // The JWT payload is passed into the verify callback
+    passport.use(new JwtStrategy(options, function(jwt_payload, done) {
+
+        // Since we are here, the JWT is valid!
+        
+        // We will assign the `sub` property on the JWT to the database ID of user
+        User.findOne({_id: jwt_payload.sub}, function(err, user) {
+            
+            // This flow look familiar?  It is the same as when we implemented
+            // the `passport-local` strategy
+            if (err) {
+                return done(err, false);
+            }
+            if (user) {
+                
+                // Since we are here, the JWT is valid and our user is valid, so we are authorized!
+                return done(null, user);
+            } else {
+                return done(null, false);
+            }
+            
+        });
+        
+    }));
+}
