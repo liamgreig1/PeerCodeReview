e3e26d550c5b6cc8801f1a9c545894527d09b8c9
liamgreig1 <liam.greig1998@googlemail.com>
2021-02-15 13:51:52 +0000

    Implemeneted Login function

--- api/controllers/controller.js
+++ api/controllers/controller.js
@@ -1,59 +1,12 @@
 'use strict';
 
-// const passport = require('passport');
+const passport = require('passport');
 const utils = require('../../lib/utils');
 const mongoose = require('mongoose');
 const User = mongoose.model('user');
 const passwordStrength = require('check-password-strength');
 const { schema } = require('../models/model');
-
-exports.list_all_users = function(req, res) {
-
-    User.find({}, function(err, user){
-        if(err){
-            res.send(err);
-        }
-        res.json(user);
-    });
-};
-
-function checkUser(uname, callback) {
-    User.countDocuments({username:uname}, function(err, user){
-        callback(err, !! user);
-    })
-}
-
-function checkEmail(email, callback) {
-    User.countDocuments({email:email}, function(err, user){
-        callback(err, !! user);
-    })
-}
-
-exports.doesUserExist = function(req, res){
-    checkUser(req.body.username, function(err,exists){
-        if(err){
-            res.json({ success: false, msg: err });
-        }
-        if(exists){
-            return res.json({ msg: true });
-        }else{
-            return res.json({ msg: false });
-        }
-    })
-}
-
-exports.doesEmailExist = function(req, res){
-    checkEmail(req.body.email, function(err,exists){
-        if(err){
-            res.json({ success: false, msg: err });
-        }
-        if(exists){
-            return res.json({ msg: true });
-        }else{
-            return res.json({ msg: false });
-        }
-    })
-}
+const router = require('express').Router();   
 
 exports.create_a_user = function(req,res, err) {
 
@@ -88,3 +41,74 @@ exports.create_a_user = function(req,res, err) {
     }
 
 };
+
+exports.login = function (req, res, next){
+    User.findOne({username: req.body.username})
+    .then((user) => {
+
+        if (!user) {
+            res.status(401).json({ success: false, msg: "could not find user" });
+        }
+        
+        // Function defined at bottom of app.js
+        const isValid = utils.validPassword(req.body.password, user.hash, user.salt);
+        
+        if (isValid) {
+
+            const tokenObject = utils.issueJWT(user);
+
+            res.status(200).json({ success: true, token: tokenObject.token, expiresIn: tokenObject.expires });
+
+        } else {
+
+            res.status(401).json({ success: false, msg: "you entered the wrong password" });
+
+        }
+
+    })
+    .catch((err) => {   
+        next(err);
+    });
+}
+
+exports.protected = function(req, res, next) {
+    res.status(200).json({ success: true, msg: "You are successfully authenticated to this route!"});
+}
+
+exports.doesUserExist = function(req, res){
+    checkUser(req.body.username, function(err,exists){
+        if(err){
+            res.json({ success: false, msg: err });
+        }
+        if(exists){
+            return res.json({ msg: true });
+        }else{
+            return res.json({ msg: false });
+        }
+    })
+}
+
+exports.doesEmailExist = function(req, res){
+    checkEmail(req.body.email, function(err,exists){
+        if(err){
+            res.json({ success: false, msg: err });
+        }
+        if(exists){
+            return res.json({ msg: true });
+        }else{
+            return res.json({ msg: false });
+        }
+    })
+}
+
+function checkUser(uname, callback) {
+    User.countDocuments({username:uname}, function(err, user){
+        callback(err, !! user);
+    })
+}
+
+function checkEmail(email, callback) {
+    User.countDocuments({email:email}, function(err, user){
+        callback(err, !! user);
+    })
+}
--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,13 +1,16 @@
 'use strict';
+const passport = require('passport');
 module.exports = function(app) {
   var user = require('../controllers/controller');
 
-  app.route('/listofusers')
-    .get(user.list_all_users);
   app.route('/register')
     .post(user.create_a_user);
   app.route('/doesUserExist')
     .post(user.doesUserExist)
   app.route('/doesEmailExist')
     .post(user.doesEmailExist)
+  app.route('/login')
+    .post(user.login)
+  app.route('/protected')
+    .get(user.protected, passport.authenticate('jwt', { session: false }))
 };
