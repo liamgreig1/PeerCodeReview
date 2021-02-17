bbed6782d033df892676d8c66d79bdf550bed4cd
liamgreig1 <liam.greig1998@googlemail.com>
2021-02-17 13:28:14 +0000

    Updated routes to fixed protected bug

---
+++ api/routes/user.js
@@ -0,0 +1,99 @@
+const mongoose = require('mongoose');
+const router = require('express').Router();   
+const User = mongoose.model('user');
+const passport = require('passport');
+const passwordStrength = require('check-password-strength');
+const utils = require('../../lib/utils');
+
+/**
+ * To log user into the application and assign JavaScript Web Token for authentication purposes
+ * @param {object} req Json object from route
+ * @param {object} res Json object which contains outcome of request
+ * @param {function} next If login fails then the next function keeps
+ *  the request moving and doesn't just let the request timeout or hang for a long time.
+ */
+// http://localhost:3000/user/login
+router.post('/login', function(req, res, next){
+    User.findOne({username: req.body.username})
+    .then((user) => {
+
+        if (!user) {
+            res.status(401).json({ success: false, msg: "username or password is wrong" });
+        }
+        
+        // Function defined at bottom of app.js
+        const isValid = utils.validPassword(req.body.password, user.hash, user.salt);
+        
+        if (isValid) {
+
+            const tokenObject = utils.issueJWT(user);
+
+            res.status(200).json({ success: true, token: tokenObject.token, expiresIn: tokenObject.expires });
+
+        } else {
+
+            res.status(401).json({ success: false, msg: "username or password is wrong" });
+
+        }
+
+    })
+    .catch((err) => {   
+        next(err);
+    });
+});
+
+/**
+ * Creates new user in database as long as user does not already exist,
+ * also that the password meets the requirements
+ * @param {object} req Json object from route
+ * @param {object} res Json object which contains outcome of request
+ */
+// http://localhost:3000/user/register
+router.post('/register', function(req, res, next){
+    User.findOne({username: req.body.username})
+    .then((user) => {
+        if (user) {
+            res.status(401).json({ success: false, msg: "User already exists" });
+        }else{
+            if(passwordStrength(req.body.password).id <= 1){
+                res.status(401).json({ success: false, msg: "Password entered is considered to be weak. " +
+                 "Password must contain At least 1 lowercase alphabetical character. 1 upper case alphabetical character. " +
+                 "Numceric character. Special character and must be longer than 8 characters" });
+            }
+        
+            const saltHash = utils.genPassword(req.body.password);
+            
+            const salt = saltHash.salt;
+            const hash = saltHash.hash;
+        
+            const newUser = new User({
+                username: req.body.username,
+                hash: hash,
+                salt: salt,
+                reputationscore: req.body.score,
+                title: req.body.title
+            });
+            try {
+                newUser.save()
+                    .then((user) => {
+                        res.status(200).json({ success: true, user: user });
+                    });
+        
+            } catch (err) {
+                res.status(401).json({ success: false, msg: err });
+            
+            }
+        }
+    })
+});
+
+/**
+ * Provides means of authentication of a user to allow them to access protected resources
+ * @param {object} req Json object from route
+ */
+// http://localhost:3000/user/protected
+router.get('/protected', passport.authenticate('jwt', { session: false }), (req, res, next) => {
+    res.status(200).json({ success: true, msg: "You are successfully authenticated to this route!"});
+});
+
+module.exports = router;
---
+++ test/protected.js
@@ -0,0 +1,88 @@
+//During the test the env variable is set to test
+process.env.NODE_ENV = 'test';
+
+let mongoose = require("mongoose");
+let User = require('../api/models/model');
+
+//Require the dev-dependencies
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../server');
+const { describe } = require("mocha");
+let should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('user', () => {
+    beforeEach((done) => { //Before each test we empty the database
+        User.deleteMany({}, (err) => {
+           done();
+        });
+    })
+    describe('/GET protected', () => {
+        it('Normal test to see if user is logedin', (done) => {
+            let user = {
+                username: "lgreig200",
+                password: "P4sSw0Rd!",
+                score: 0,
+                title: "Student"
+            }
+            chai.request(server)
+                .post('/user/register')
+                .send(user)
+                .end((err, res) => {
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('success').eql(true);
+                    chai.request(server)
+                        .post('/user/login')
+                        .send(user)
+                        .end((err, res) => {
+                            res.body.should.be.a('object');
+                            res.body.should.have.property('success').eql(true);
+                            var token = res.body.token;
+                            chai.request(server)
+                                .get('/user/protected')
+                                .set('Authorization', token)
+                                .end((err, res) => {
+                                    res.body.should.be.a('object');
+                                    res.body.should.have.property('success').eql(true);
+                                    res.body.should.have.property('msg').eql('You are successfully authenticated to this route!');
+                                    done();
+                                });
+                        });
+                });
+        });
+    });
+
+    describe('/GET protected', () => {
+        it('Exceptional test make sure only user with token can access this route', (done) => {
+            let user = {
+                username: "lgreig200",
+                password: "P4sSw0Rd!",
+                score: 0,
+                title: "Student"
+            }
+            chai.request(server)
+                .post('/user/register')
+                .send(user)
+                .end((err, res) => {
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('success').eql(true);
+                    chai.request(server)
+                        .post('/user/login')
+                        .send(user)
+                        .end((err, res) => {
+                            res.body.should.be.a('object');
+                            res.body.should.have.property('success').eql(true);
+                            chai.request(server)
+                                .get('/user/protected')
+                                .set('Authorization', 'noToken')
+                                .end((err, res) => {
+                                    res.should.have.status(401);
+                                    done();
+                                });
+                        });
+                });
+        });
+    });
+});
