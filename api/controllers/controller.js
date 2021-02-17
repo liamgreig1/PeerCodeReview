aea2dbf2330b0825ba59903b91ecf61a06fa0f2b
liamgreig1 <liam.greig1998@googlemail.com>
2021-02-17 11:36:53 +0000

    Added Comments

--- api/controllers/controller.js
+++ api/controllers/controller.js
@@ -8,14 +8,22 @@ const passwordStrength = require('check-password-strength');
 const { schema } = require('../models/model');
 const router = require('express').Router();   
 
-exports.create_a_user = function(req,res, err) {
+/**
+ * Creates new user in database as long as user does not already exist,
+ * also that the password meets the requirements
+ * @param {object} req Json object from route
+ * @param {object} res Json object which contains outcome of request
+ */
+exports.create_a_user = function(req,res) {
     User.findOne({username: req.body.username})
         .then((user) => {
             if (user) {
                 res.status(401).json({ success: false, msg: "User already exists" });
             }else{
                 if(passwordStrength(req.body.password).id <= 1){
-                    res.status(401).json({ success: false, msg: "Password entered is considered to be weak. Password must contain At least 1 lowercase alphabetical character. 1 upper case alphabetical character. Numceric character. Special character and must be longer than 8 characters" });
+                    res.status(401).json({ success: false, msg: "Password entered is considered to be weak. " +
+                     "Password must contain At least 1 lowercase alphabetical character. 1 upper case alphabetical character. " +
+                     "Numceric character. Special character and must be longer than 8 characters" });
                 }
             
                 const saltHash = utils.genPassword(req.body.password);
@@ -44,6 +52,13 @@ exports.create_a_user = function(req,res, err) {
         })
 };
 
+/**
+ * To log user into the application and assign JavaScript Web Token for authentication purposes
+ * @param {object} req Json object from route
+ * @param {object} res Json object which contains outcome of request
+ * @param {function} next If login fails then the next function keeps
+ *  the request moving and doesn't just let the request timeout or hang for a long time.
+ */
 exports.login = function (req, res, next){
     User.findOne({username: req.body.username})
     .then((user) => {
@@ -73,6 +88,10 @@ exports.login = function (req, res, next){
     });
 }
 
-exports.protected = function(req, res, next) {
+/**
+ * Provides means of authentication of a user to allow them to access protected resources
+ * @param {object} req Json object from route
+ */
+exports.protected = function(res) {
     res.status(200).json({ success: true, msg: "You are successfully authenticated to this route!"});
 }
